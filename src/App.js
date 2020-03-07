b45a8558d20447fdefdb59c3ed5cc0e226d07dc6
Filitaryn Alex <ikindi@mail.ru>
2020-03-07 11:26:00 +0200

    init: start boilerplate

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header-wrapper">
+        <div className="header-container">
+          header
+        </div>
+      </header>
+      <main className="main-wrapper">
+        <div className="main-container">
+          main
+        </div>
+      </main>
+      <footer className="footer-wrapper">
+        <div className="footer-container">
+          footer
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
