be8efd4fcce7201b6f3224091198efd98e8ca65b
Filitaryn Alex <ikindi@mail.ru>
2020-04-18 16:12:34 +0300

    Updates

---
+++ precache-manifest.bd63446ab11fcfe7756c08177f658da6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0018953a0e85fc254f429d790d6b6ab6",
+    "url": "/flosta/index.html"
+  },
+  {
+    "revision": "ec007a294e65858adb5a",
+    "url": "/flosta/static/css/main.680696d2.chunk.css"
+  },
+  {
+    "revision": "710bd9c5229699ecd504",
+    "url": "/flosta/static/js/2.b25c230b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/flosta/static/js/2.b25c230b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec007a294e65858adb5a",
+    "url": "/flosta/static/js/main.139fbd19.chunk.js"
+  },
+  {
+    "revision": "b44feebb5595698979ea",
+    "url": "/flosta/static/js/runtime-main.d7824933.js"
+  }
+]);
