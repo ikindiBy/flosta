1b1c4f94b9930db96d4ee0e7e584473b39271e87
Filitaryn Alex <ikindi@mail.ru>
2020-05-27 01:21:03 +0300

    fix static

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use('/api/auth', require('./routes/auth.routs'));
 
 if (process.env.NODE_ENV === 'production') {
-    app.use('/', app.static(path.join(__dirname, 'client', 'build')));
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
